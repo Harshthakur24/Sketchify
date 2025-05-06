c76217b6d46c9fb92591ed2dd899a4eee036263a
Harshthakur24 <thakur2004harsh@gmail.com>
2025-05-06 23:13:28 +0530

    update: add @vitejs/plugin-react as a development dependency and remove Vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: './client', // tell Vite where the index.html is
   plugins: [react()],
 })
