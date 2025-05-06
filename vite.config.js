e302e2ffdebb8468dcedf80b9169e48a6b78de14
Harshthakur24 <thakur2004harsh@gmail.com>
2025-05-06 23:19:48 +0530

    update: modify Vite configuration to set output directory and enable empty output directory option

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './client', // tell Vite where the index.html is
-  plugins: [react()],
+  root: 'client',
+  build: {
+    outDir: '../dist', // puts output in root/dist
+    emptyOutDir: true
+  },
+  plugins: [react()]
 })
